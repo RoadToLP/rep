4157362240eb4891082b585cf1b86eb2c245fd70
RoadToLP <ilya.titoff2013@yandex.ru>
2016-10-13 16:24:37 +0300

    Added CLASS

--- modSqrX.cpp
+++ modSqrX.cpp
@@ -15,12 +15,13 @@ int main(){
 		cout << "Two answers here" << endl;
 		two = true;
 	}
-	float x1 = (((-1)*b-sqrt(d))/(2*a)), x2 = (((-1)*b+sqrt(d))/(2*a));
+	float x1 = (((-1)*b+sqrt(d))/(2*a)), x2 = (((-1)*b-sqrt(d))/(2*a));
 	if (two){
 		cout << "X1: " << x1 << " X2: " << x2 << endl;
 	} else {
 		cout << "X1: " << x1 << endl;
 	}
+	cout << "Discriminant: " << d;
 	float peakX = (((-1)*b)/(2*a));
 	cout << "Peak: X = " << peakX << "; Y = " << (pow(peakX, 2) * a) + (peakX * b) + c << endl;
 	return 0;
